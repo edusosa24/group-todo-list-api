71c8a0651032fec0c2b186368dc4b9f46575c453
Eduardo Sosa <edu_33@live.com.ar>
2022-06-26 00:50:45 -0300

    Setted group routes structure

---
+++ routes/groups.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  getGroups,
+  getGroup,
+  createGroup,
+  updateGroup,
+  deleteGroup,
+} = require('../controllers/groups');
+
+router.route('/').get(getGroups).post(createGroup);
+
+router.route('/:id').get(getGroup).put(updateGroup).delete(deleteGroup);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Importing routes
+const groups = require('./routes/groups');
+
+// Loading env variables
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+//                                ROUTES
+
+app.use('/api/v1/groups', groups);
+
+//                        -------------------------
 
 app.listen(PORT, console.log(`MODE:${process.env.NODE_ENV}  PORT:${PORT}`));
